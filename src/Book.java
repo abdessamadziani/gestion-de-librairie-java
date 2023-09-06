c6a2202e46b2c0d80caeda69b12badec645feabd
abdssamad <zianiabdssamad30@gmail.com>
2023-09-06 10:58:36 +0100

    add methodes add ,delete , get

--- src/Book.java
+++ src/Book.java
@@ -11,35 +11,132 @@ public class Book {
     public int id_auteur;
     public int id_gestionnaire;
     public int id_emprunteur;
-    Book(String titre,String isbn,String auteur,String statut,int id_auteur,int id_gestionnaire,int id_emprunteur)
+    Book(String titre,String statut,String isbn,int id_gestionnaire,int id_auteur,int id_emprunteur)
     {
         this.titre=titre;
-        this.isbn=isbn;
-        this.auteur=auteur;
         this.statut=statut;
-        this.id_auteur=id_auteur;
+        this.isbn=isbn;
         this.id_gestionnaire=id_gestionnaire;
+        this.id_auteur=id_auteur;
         this.id_emprunteur=id_emprunteur;
+    }
+    Book(String titre,String statut,String isbn,int id_gestionnaire,int id_auteur)
+    {
+        this.titre=titre;
+        this.statut=statut;
+        this.isbn=isbn;
+        this.id_gestionnaire=id_gestionnaire;
+        this.id_auteur=id_auteur;
+
     }
 
     //public static final String jdbcUrl = "jdbc:mysql://localhost:3306/library";
    // public static final String username = "root";
     //public static final String password = "";
+    public static void afficherLivres()
+    {
+        String sqlQuery = "SELECT * FROM livre";
 
+        try (Connection connection = Connect.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            // Process the result set here
+         /*   while (resultSet.next()) {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println() ;// Newline after each row
+                System.out.println(columnCount);
+
+            }*/
+
+
+            while (resultSet.next()) {
+                int columnCount = resultSet.getMetaData().getColumnCount();
+
+                // Define the border characters
+                char horizontalBorder = '-';
+                char verticalBorder = '|';
+                char cornerBorder = '+';
+
+                // Print the top border
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(cornerBorder);
+                    for (int j = 0; j <20; j++) { // Adjust the width (e.g., 20 characters)
+                        System.out.print(horizontalBorder);
+                    }
+                }
+                System.out.println(cornerBorder); // End of the top border
+
+                // Print column headers and data
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnHeader = resultSet.getMetaData().getColumnName(i);
+                    System.out.printf("%c %-18s ", verticalBorder, columnHeader); // Adjust the width (e.g., 20 characters)
+
+                }
+                System.out.println(verticalBorder); // End of the headers row
+
+                // Print rows
+                do {
+
+
+                    // Print the top border
+                    for (int k = 1; k <= columnCount; k++) {
+                        System.out.print(cornerBorder);
+                        for (int j = 0; j <20; j++) { // Adjust the width (e.g., 20 characters)
+                            System.out.print(horizontalBorder);
+                        }
+                    }
+                    System.out.println(cornerBorder); // End of the top border
+
+
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnValue = resultSet.getString(i);
+                        System.out.printf("%c %-18s ", verticalBorder, columnValue); // Adjust the width (e.g., 20 characters)
+                    }
+                    System.out.println(verticalBorder); // End of each data row
+
+                } while (resultSet.next());
+
+                // Print the bottom border
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(cornerBorder);
+                    for (int j = 0; j < 20; j++) { // Adjust the width (e.g., 20 characters)
+                        System.out.print(horizontalBorder);
+                    }
+                }
+                System.out.println(cornerBorder); // End of the bottom border
+            }
+
+
+
+            Connect.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
    // public static boolean addLivre(String titre, String statut, String isbn, int id_gestionnaire, int id_auteur, int id_emprunteur)
    public static boolean addLivre(Book book) {
        String insertQuery = "INSERT INTO livre (title, statut, isbn, id_gestionnaire, id_auteur, id_emprunteur) VALUES (?, ?, ?, ?, ?, ?)";
 
-       try (Connection connection2 = Connect.getConnection();
-            PreparedStatement preparedStatement = connection2.prepareStatement(insertQuery)) {
-
+       try (Connection connection = Connect.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
            preparedStatement.setString(1, book.titre);
            preparedStatement.setString(2, book.statut);
            preparedStatement.setString(3, book.isbn);
            preparedStatement.setInt(4, book.id_gestionnaire);
            preparedStatement.setInt(5, book.id_auteur);
-           preparedStatement.setInt(6, book.id_emprunteur);
+           //preparedStatement.setInt(6, book.id_emprunteur == 0 ? null : book.id_emprunteur);
+           // Handle id_emprunteur
+           if (book.id_emprunteur == 0) {
+               preparedStatement.setNull(6, Types.INTEGER);
+           } else {
+               preparedStatement.setInt(6, book.id_emprunteur);
+           }
 
            int rowsAffected = preparedStatement.executeUpdate();
            Connect.closeConnection();
@@ -49,6 +146,22 @@ public class Book {
            return false;
        }
    }
+
+
+    public static boolean supprimerLivre(String isbn) {
+        String sqlQuery = "DELETE FROM livre WHERE isbn=?";
+
+        try (Connection connection = Connect.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            preparedStatement.setString(1, isbn);
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0; // Return true if a row was deleted
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,69 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hello \n");
-        System.out.println("My name is Samad \n");
 
-        int choix;
+        int choix,id_gestionnaire,id_auteur,id_emprunteur ;
+        boolean success;
+        String titre,isbn,statut;
         do {
             System.out.println("\t\t\t\t\n:::::::::::::::::::::::::: Menu Principale ::::::::::::::::::::::::::\n\n");
             System.out.println("\t\t\t 1- Introduire un livre ");
             System.out.println("\t\t\t 2- Introduire plusieurs livres ");
             System.out.println("\t\t\t 3- Emprunté un livre ");
             System.out.println("\t\t\t 4- Affichage ");
-            System.out.print("\t\t\t 5- Quitter L'application\n\n");
+            System.out.println("\t\t\t 5- Supprimer un livre ");
+            System.out.print("\t\t\t 6- Quitter L'application\n\n");
             System.out.print("\t\t\t Donnez votre choix: ");
             choix = input.nextInt();
             input.nextLine();
-        } while (choix != 5);
+            switch (choix)
+            {
+                case 1:
+                    System.out.println("donner titre de livre");
+                    titre = input.nextLine();
+                    System.out.println("donner statut de livre");
+                    statut = input.nextLine();
+                    System.out.println("donner isbn de livre");
+                    isbn = input.nextLine();
+                    System.out.println("donner id_gestionnaire");
+                     id_gestionnaire= input.nextInt();
+                    System.out.println("donner id_auteur");
+                    id_auteur = input.nextInt();
+                    Book bk = new Book(titre,"disponible", isbn, id_gestionnaire, id_auteur);
+                     success = Book.addLivre(bk);
+                    if (success) {
+                        System.out.println("Livre added successfully.");
+                    } else {
+                        System.out.println("Failed to add the livre.");
+                    }
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    Book.afficherLivres();
+                    break;
+                case 5:
+                    String is;
+                    System.out.println("donner le isbn de livre que vous voulez supprimer");
+                    is = input.nextLine();
+                    success = Book.supprimerLivre(is);
+                    if (success) {
+                        System.out.println("Livre supprimer successfully.");
+                    } else {
+                        System.out.println("Failed to  the livre.");
+                    }
+                    break;
+                default:
+
+            }
+        } while (choix != 6);
+
+
         System.out.print("\t\t\t Fin de programme ... \n\n");
 
-        try (Connection connection = Connect.getConnection()) {
-            String sqlQuery = "SELECT * FROM livre";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-            // Process the result set here
-            while (resultSet.next()) {
-                int columnCount = resultSet.getMetaData().getColumnCount();
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(resultSet.getString(i) + "\t");
-                }
-                System.out.println(); // Newline after each row
-            }
-            Connect.closeConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        Book bk = new Book("book mehdi", "xxxx", "samad2", "pas disponible2", 1, 1, 1);
 
 
 
@@ -48,12 +75,11 @@ public class Main {
 
 
 
-            boolean success = Book.addLivre(bk);
-            if (success) {
-                System.out.println("Livre added successfully.");
-            } else {
-                System.out.println("Failed to add the livre.");
-            }
+
+
+
+
+
 
 
 
